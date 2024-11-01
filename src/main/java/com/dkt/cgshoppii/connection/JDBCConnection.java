4d96522627ff4a1f03bfb87c5092e52277ddcd67
nguyenminhtuan100802 <139602897+nguyenminhtuan100802@users.noreply.github.com>
2024-11-01 14:57:32 +0700

    1/11/24 - Create function get connection for JDBCConnection in Utility

--- src/main/java/com/dkt/cgshoppii/connection/JDBCConnection.java
+++ src/main/java/com/dkt/cgshoppii/connection/JDBCConnection.java
@@ -1,4 +1,23 @@
 package com.dkt.cgshoppii.connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JDBCConnection {
+    public static Connection getConnection() {
+        Connection connection = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+
+            String jdbcURL = "jdbc:mysql://localhost:3306/bank_management";
+            String jdbcUsername = "root";
+            String jdbcPassword = "minhtuan123";
+
+            connection = DriverManager.getConnection(jdbcURL, jdbcUsername, jdbcPassword);
+        } catch (SQLException | ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
 }
